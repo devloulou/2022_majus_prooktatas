078c0bf16e825ecad70e697972b58cb5298b99a1
Richard Kovacs <dev.kovacs.richard90@gmail.com>
2022-06-27 20:41:18 +0200

    test

---
+++ 9. alkalom/next.py
@@ -0,0 +1,12 @@
+"""
+háromrétegű architektúra
+
+mi az az adatbázis
+mi az relációs adatbázis
+mi az a session
+mi az a tranzakció
+adatbázis objektumok
+alapvetó sql utasítások csoportosítása
+ezekre példa
+
+"""
--- test.py
+++ test.py
@@ -1,14 +1,25 @@
-dict1 = {'a': 10, 'b': 8}
-dict2 = {'d': 6, 'c': 4}
-dict3 = {'e': 6, 'f': 4}
-dict4 = {'g': 6, 'h': 4}
+my_list = [1, 2, 3, 4, 5, 6, 7]
 
-def my_func(*args):
-    import pprint 
-    my_temp_dict = {}
-    for item in args:
-        my_temp_dict.update(item)
+print(my_list[2::2])
 
-    pprint.pprint(my_temp_dict, indent=4)
 
-my_func(dict1, dict2, dict3, dict4)
+
+def my_func(a, b):
+    """
+    This function powered double the a value then add
+    to b, because this need to 
+    """
+    a *= 2
+    return a + b
+
+#clean_code változat
+def double_a(a):
+    return a*2
+
+def my_func(a, b):
+    return double_a(a) + b
+
+
+if __name__ == '__main__':
+    sol = my_func(2, 3)
+    print(sol)
