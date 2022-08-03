3eff1d60cbe57b29743e6d86f381640169e04fe0
Richard Kovacs <dev.kovacs.richard90@gmail.com>
2022-08-03 20:15:27 +0200

    20. alkalom

---
+++ 20. alkalom/virtual_environment.py
@@ -0,0 +1,11 @@
+"""
+Mi az a virtuális környezet? Anaconda, virtualenv, pyenv
+Mire jó?
+Miért használjuk?
+
+Virtuális környezet: egy olyan mapparendszer, a rendszeredtől függetlenül
+tudsz magadnak fejlesztői környezetet kialakítani
+
+Fejlesztői környezet: python verzió és a libraryk / modulok
+
+"""
